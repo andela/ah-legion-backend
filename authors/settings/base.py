3b51f22e6b87867ff9f85db5d433eca84fcaa0cd
Brian Serem <brianseremiat@gmail.com>
2019-02-26 15:11:42 +0300

    chore(heroku) : deploy base application to heroku
    -create procfile
    -create a runtime.txt file with current python build
    -install whitenoise
    -configure whitenoise in settings.py
    -add .DS_Store to gitignore
    -add allowed hosts in settings.prod module
    -install gunicorn
    -update readme.md to reflect recent changes
    -add static root url to path
    -add release intructions to Procfile
    
    [Finishes #164046242]

--- authors/settings/base.py
+++ authors/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7pgozr2jn7zs_o%i8id6=rddie!*0f0qy3$oy$(8231i^4*@u3'
+SECRET_KEY = config('SECRET_KEY', default = '#dfhewy73twy3e88Y@Y828Y@91')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
