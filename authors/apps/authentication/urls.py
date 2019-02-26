3d883a139017723e91bb0c185a5fef5806d210cb
Keith Mandela <kazi.keith@gmail.com>
2019-02-26 11:39:23 +0300

    chore(tests): write unit tests for implemented functionality
    - write unit tests for models
    - write unit tests for views
    - write unit tests for serializers
    - install coverage to get coverage and create .coveragerc file to configure ignored files
    - add scripts for creating migrations on TravisCI database
    [finishes #164046234]

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -3,11 +3,10 @@ from django.urls import path
 from .views import (LoginAPIView, RegistrationAPIView,
                     UserRetrieveUpdateAPIView)
 
-
-app_name = "authentication"
+app_name = 'authentication'
 
 urlpatterns = [
-    path('user/', UserRetrieveUpdateAPIView.as_view()),
-    path('users/', RegistrationAPIView.as_view()),
-    path('users/login/', LoginAPIView.as_view()),
+    path('user/', UserRetrieveUpdateAPIView.as_view(), name='get users'),
+    path('users/', RegistrationAPIView.as_view(), name='register'),
+    path('users/login/', LoginAPIView.as_view(), name='login'),
 ]
